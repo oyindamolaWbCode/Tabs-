c92ab117d7b7f33d789ab2828fd6beb89b6634f5
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2024-01-14 18:59:16 +0100

    modified

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ let tabContainer = document.querySelector('ol')
 
 const tabContent = [{
     title : 'Features',
-    icon: 'fa-circle-check',
+    icon: 'fa-regular fa-circle-check',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium explicabo nam provident non, dolorum recusandae sequi ipsam soluta voluptatum. Natus voluptatum obcaecati quasi tenetur cumque necessitatibus odio ab tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptates, iure natus maiores ducimus quaerat blanditiis provident, et id ut non ad velit cum consectetur modi facilis doloremque sit? Ut'
 },
 {
     title: 'Delivery cont...',
-    icon: 'fa-truck',
+    icon: 'fa-regular fa-truck',
     text: 'CSS tabs are used to help users navigate websites with multiple pages. They use HTML for structure and CSS for styling Here are some steps for creating responsive tabs with CSSStructure your HTML: Define a section for tabs and tab content in your HTML file'
 },
 {
  title: 'Shipping',
- icon: 'fa-truck-fast',
+ icon: 'fa-regular fa-truck-fast',
  text: 'non, dolorum recusandae sequi ipsam soluta voluptatum. Natus voluptatum obcaecati quasi tenetur cumque necessitatibus odio ab tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptates, iure natus maiores ducimus quaerat blanditiis provident, et id ut non ad velit cum consectetur modi facilis doloremque sit? Ut'
 },
 {
     title: 'Returns',
-    icon: 'fa-person-walking-arrow-loop-left',
+    icon: 'fa-regular fa-person-walking-arrow-loop-left',
     text: 'non, dolorum recusandae sequi ipsam soluta voluptatum. Natus voluptatum obcaecati quasi tenetur cumque necessitatibus odio ab tempore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptates, iure natus maiores ducimus quaeratCSS tabs are used to help users navigate websites with multiple pages. They use HTML for structure'
 }
 ]
